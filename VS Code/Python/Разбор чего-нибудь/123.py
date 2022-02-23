66238cb5e5427133bfe73188a6d055cd9c3f293f
Dadqe <ru9514120022@gmail.com>
2022-02-24 01:49:23 +0600

    add

--- VS Code/Python/Разбор чего-нибудь/123.py
+++ VS Code/Python/Разбор чего-нибудь/123.py
@@ -1,9 +1,18 @@
 # """Что-то с генераторами, переменная b - генератор, которая генерирует, похоже, список, но type = class 'generator' и потом итерируемся по этому генератору"""
+import sys
+# from a import a
+sys.path.insert(0, 'E:/Tokens/KinoPoisk/')
+from Token import token
 
-# a = [1,2,3,4]
+# sys.path.insert(0, 'E:/Work/VS Code/Python/')
+
+
+# # a = [1,2,3,4]
 # b = (2*x for x in a)
 # for i in b:
 #     print(i, end=' ')
 
 # print("finally")
 # print('commit')
+
+print(type(token))
